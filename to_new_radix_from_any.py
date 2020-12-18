27ab509e49787c1654a0e83e9449cd10eddf779f
lev <levgeneral@yandex.ru>
2020-12-18 23:02:43 +0300

    ADD Added simple stupidman protection

--- to_new_radix_from_any.py
+++ to_new_radix_from_any.py
@@ -5,6 +5,11 @@ notation. (from 1th to 36th)
 import argparse
 import sys
 
+def error_message ():
+    print('What are you doing man? You have entered THE WRONG RADIX!!!!! >:E')
+    sys.exit()
+
+
 def get_key (dict, val): # Function to get dictionary key by its value
     for k, v in dict.items():
         if v == val:
@@ -55,10 +60,9 @@ parser.add_argument('--n', default=1, type=str, help='This argument requires num
 parser.add_argument('--r', type=int, required=True, help='This argument requires number radix.')
 parser.add_argument('--tr', default=1, type=int, help='This argument requires radix to translate into.')
 args = parser.parse_args()
-n = args.n
+n = args.n.upper()
 r = args.r
 tr = args.tr
-
 alphabet = {
 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H',
 18: 'I', 19: 'J', 20: 'K', 21: 'L', 22: 'M', 23: 'N', 24: 'O', 25: 'P',
@@ -66,7 +70,25 @@ alphabet = {
 34: 'Y', 35: 'Z'
 }
 
-if not n.isdigit():
-    pass
+if n.isalnum() and str(r).isalnum() and str(tr).isalnum():
+    if tr > 36 or r > 36:
+        error_message()
+
+    if not n.isdigit(): # Checking if number exceeds entered radix
+        literals = n
+        for i in literals:
+            if i in '0123456789':
+                literals = literals.replace(i, '')
+        literals = list(''.join(sorted(literals)))
+        max_literal = literals[-1]
+        max_litnum = get_key(alphabet, max_literal)
+        if r > max_litnum:
+            print(new_radix_from_any(n, r, tr))
+        else:
+            error_message()
+    else:
+        print(new_radix_from_any(n, r, tr))
+
 else:
-    print(new_radix_from_any(n, r, tr))
+    print('Please delete redundant symbols!')
+    sys.exit()
